3d04e04fb77dc87139bcf7f65803fe509052ff0b
Michelle <appel.michelle@gmail.com>
2018-03-03 10:11:13 +0100

    TODO: mirror image

--- rotate_harris.m
+++ rotate_harris.m
@@ -1,10 +1,15 @@
 function [ H, r, c ] = rotate_harris(image, threshold, window_size, angle)
 
-if nargin == 3
+if nargin < 4
     angle = rand*360;
     disp(angle)
 end
 
+if nargin == 2
+    window_size = 26;
+end
+
+% TODO: Instead of zero padding mirror image
 image = imrotate(image, angle);
 [ H, r, c ] = harris_corner_detector(image, threshold, window_size);
 
