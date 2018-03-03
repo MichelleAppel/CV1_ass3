461e21cff4b2f5bf5f631351883ade67b38d9446
Michelle <appel.michelle@gmail.com>
2018-03-03 09:43:49 +0100

    random angle

--- rotate_harris.m
+++ rotate_harris.m
@@ -1,6 +1,10 @@
-function [ H, r, c ] = rotate_harris(image, angle, threshold, window_size)
+function [ H, r, c ] = rotate_harris(image, threshold, window_size, angle)
 
-% TODO: random angle
+if nargin == 3
+    angle = rand*360;
+    disp(angle)
+end
+
 image = imrotate(image, angle);
 [ H, r, c ] = harris_corner_detector(image, threshold, window_size);
 
