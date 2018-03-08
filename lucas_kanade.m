ec03a52f680c88ce78638d292045b8040628a735
Michelle <appel.michelle@gmail.com>
2018-03-08 13:09:35 +0100

    movie G

--- lucas_kanade.m
+++ lucas_kanade.m
@@ -44,7 +44,54 @@ image2_regions = mat2cell(image2, rowDivision, columnDivision);
 % Then, estimate optical flow as given in Equation 20.
 
 % FOR EACH REGION, CALCULATE THE OPTICAL FLOW
-flow_vectors = solve_flow_vectors(image1_regions, image2_regions);
+% FOR EACH REGION, CALCULATE THE OPTICAL FLOW
+[ row_regions, column_regions ] = size(image1_regions);
+
+[ regionHeight, regionWidth ] = size(cell2mat(image1_regions(1)));
+
+flow_vectors = zeros(row_regions * column_regions, 4);
+counter = 1;
+for i = 1:row_regions
+    for j = 1:column_regions
+        im1region = cell2mat(image1_regions(i, j));
+        im2region = cell2mat(image2_regions(i, j));
+
+        [ h, w ] = size(im1region);
+        
+        % Incoming fugly piece of code to apply that Gauss
+        G = gauss2D(20 , max(regionHeight, regionWidth));
+
+        % Make matching dimensions
+        if regionHeight ~= h
+            b = floor((regionHeight - h)/2);
+            G = G( b:b+h - 1 ,:);
+        end
+        
+        if regionWidth ~= w
+            b = floor((regionWidth - w)/2);
+            G = G(:, b:b+w-1 );
+        end        
+       
+        im1region = G .* double(im1region); % Apply
+        im2region = G .* double(im2region); % Apply
+        
+        [ Gx, Gy ] = imgradientxy(im1region);  % Compute the gradients wrt x & y
+        Gt = im1region - im2region;           % Compute the gradients wrt t
+        
+        A(:, 1) = double(reshape(Gx, h*w, 1));
+        A(:, 2) = double(reshape(Gy, h*w, 1)); 
+        b       = double(reshape(Gt, h*w, 1));
+        v = (transpose(A) * A) \ (transpose(A) * b);
+        
+        A = [];  % reset to prevent dimension error
+        
+        avg_y_pixel = i*regionHeight-0.5*h;
+        avg_x_pixel = j*regionWidth-0.5*w;
+        
+        flow_vectors(counter, :) = [avg_x_pixel, avg_y_pixel, v(1), v(2)];
+        counter = counter + 1;
+    end
+end
 
 figure, imshow(image1)
 hold on;
--- tracking.m
+++ tracking.m
@@ -1,10 +1,13 @@
 function tracking(directory_name, regionWidth, regionHeight)
 
+close ALL
+
 if nargin == 1
-    regionWidth = 15;
-    regionHeight = 15;
+    regionWidth = 25;
+    regionHeight = 25;
 end
 
+% Import all images from directory
 D = directory_name;
 S = dir(fullfile(D,'*.jpg'));
 imgCell = cell(numel(S));
@@ -13,18 +16,71 @@ for k = 1:numel(S)
     imgCell{k} = imread(file);
 end
 
+% Step 1: Locate feature points on first image
 first_image = imgCell{1};
-[ ~, r, c ] = harris_corner_detector(first_image, 26, 0.02);
+[ ~, r, c ] = harris_corner_detector(first_image, 26, 0.015);
 
-flow_vectors = zeros(2, 1, length(length(imgCell)-1));
+% Step 2: Compute flow vector for all image pairs
+flow_vectors = zeros(length(r), 4, length(length(imgCell)-1));
+
 for i = 1:length(imgCell)-1
     regions_image_1 = get_regions(imgCell{i}, r, c, regionWidth, regionHeight);
     regions_image_2 = get_regions(imgCell{i+1}, r, c, regionWidth, regionHeight);
-    flow_vectors(:, :, i) = solve_flow_vectors(regions_image_1, regions_image_2);
+    flow_vectors(:, :, i) = solve_flow_vectors(regions_image_1, regions_image_2, r, c);
+    
+    
+
+    frame = figure;
+    set(frame, 'Visible', 'off');
+    imshow(imgCell{i});
+    hold on;
+    quiver(flow_vectors(:, 1, i), flow_vectors(:, 2, i), flow_vectors(:, 3, i), flow_vectors(:, 4, i), 'linewidth', 1, 'color', 'g', 'MaxHeadSize', 2);
+    M(i) = getframe;
+    close ALL
+    
+    % Update feature points
+    c = round(c + regionWidth*flow_vectors(:, 3, i));
+    r = round(r + regionHeight*flow_vectors(:, 4, i));
+    
 end
-flow_vectors
+movie(M)
+
 
 end
+
+%%
+function [ flow_vectors ] = solve_flow_vectors(image1_regions, image2_regions, r, c)
+% FOR EACH REGION, CALCULATE THE OPTICAL FLOW
+[ h, w, no_regions ] = size(image1_regions);
+
+flow_vectors = zeros(no_regions, 4);
+counter = 1;
+for i = 1:no_regions
+    im1region = image1_regions(:, :, i);
+    im2region = image2_regions(:, :, i);       
+
+    % Incoming fugly piece of code to apply that Gauss
+    G = gauss2D(20 , max(h, w));
+    
+    im1region = G .* im1region; % Apply
+    im2region = G .* im2region; % Apply
+
+    [ Gx, Gy ] = imgradientxy(im1region);  % Compute the gradients wrt x & y
+    Gt = im1region - im2region;           % Compute the gradients wrt t
+
+    A(:, 1) = double(reshape(Gx, h*w, 1));
+    A(:, 2) = double(reshape(Gy, h*w, 1)); 
+    b       = double(reshape(Gt, h*w, 1)); 
+    v = (transpose(A) * A) \ (transpose(A) * b);
+
+    A = [];  % reset to prevent dimension error
+
+    flow_vectors(counter, :) = [c(i), r(i), v(1), v(2)];
+    counter = counter + 1;
+
+end
+end
+
 %%
 function [ regions ] = get_regions(image, r, c, regionWidth, regionHeight)
 
